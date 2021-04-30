94b8a711927e6c75296e24299be4db64a4316def
Prakash Vishwakarma <pvishwa1@volvocars.com>
2021-04-30 17:19:12 +0200

    Always open chrome in 5th tag/workspace

--- config.def.h
+++ config.def.h
@@ -26,10 +26,11 @@ static const Rule rules[] = {
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
 	 */
-	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
-	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
-	{ "Teams",    NULL,       NULL,       1 << 7,       0,           -1 },
+	/* class      		instance    title       tags mask     isfloating   monitor */
+	{ "Gimp",     		NULL,       NULL,       0,            1,           -1 },
+	{ "Firefox",  		NULL,       NULL,       1 << 8,       0,           -1 },
+	{ "Teams",    		NULL,       NULL,       1 << 7,       0,           -1 },
+	{ "google-chrome",    	NULL,       NULL,       1 << 4,       0,           -1 },
 };
 
 /* layout(s) */
